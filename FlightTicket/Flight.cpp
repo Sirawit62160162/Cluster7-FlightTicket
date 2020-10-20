5600f52e78e39a375fc45aa11408d63707f28468
SippaKonC7 <73053996+SippaKonC7@users.noreply.github.com>
2020-10-20 13:25:32 +0700

    template Flight,Seat

--- FlightTicket/Flight.cpp
+++ FlightTicket/Flight.cpp
@@ -1,1 +1,18 @@
 #include "Flight.h"
+Flight Flight::reserveTicket(string flightCode){
+	
+	
+}
+void Flight::getSeat(string flightCode){
+	
+}
+void Flight::getFlight(){
+	
+}
+bool Flight::removeFlight(string flightCode){
+	
+}
+void Flight::addFlight(string start,string destination,string departureTime,string arriveTime,double price,string flightCode){
+	
+}
+
--- FlightTicket/Seat.cpp
+++ FlightTicket/Seat.cpp
@@ -1,1 +1,11 @@
 #include "Seat.h"
+
+void Seat::showSeat(){
+	
+}
+void Seat::reserveSeat(string seatId){
+	
+}
+void Seat::changeStatus(string seatId,string status){
+	
+}
