c11730f3a2a271d8801acf5cc6e7c0ce6952cb87
Nitipon094 <62160094@go.buu.ac.th>
2020-10-20 13:11:45 +0700

    Template

--- FlightTicket/ReservationCustomer.cpp
+++ FlightTicket/ReservationCustomer.cpp
@@ -1,1 +1,40 @@
 #include "ReservationCustomer.h"
+
+void ReservationCustomer::reserveTicket(string flightCode){
+	
+}
+
+void ReservationCustomer::reserveSeat(string seatId){
+	
+}
+
+void ReservationCustomer::getCustomerInformation(){
+	
+}
+
+void ReservationCustomer::createTicketId(){
+	
+}
+
+ReservationCustomer ReservationCustomer::showTicketInformation(){	
+}
+
+bool ReservationCustomer::cancelTickrtId(){
+	
+}
+
+void ReservationCustomer::setStatus(string status){
+	
+}
+
+void ReservationCustomer::showCustomer(){
+	
+}
+bool ReservationCustomer::checkinTicketId(string ticketId){
+	
+}
+
+void ReservationCustomer::reserveTicketName(string firstname,string lastname,string passportId){
+	
+}
+
