766a7070a1fdac578f0d1fd50af9327b82f9c33e
aeworaprat <73052317+aeworaprat@users.noreply.github.com>
2020-10-20 12:56:54 +0700

    template UI , controller

--- FlightTicket/ReservationController.cpp
+++ FlightTicket/ReservationController.cpp
@@ -4,3 +4,60 @@ void ReservationController::Register(string firstname,string lastname,string pas
 	Customer *test = new Customer();
 	test->saveRegister(firstname,lastname,passportId,username,password);
 }
+
+void ReservationController::login(string username,string password){
+	
+}
+
+void ReservationController::reserveTicket( string flightCode ){
+	
+}
+	
+void ReservationController::getSeat(string flightCode){
+	
+}
+	
+void ReservationController::reserveSeat(string seatId){
+
+}
+	
+void ReservationController::getCustomerinformation(){
+	
+}
+
+void ReservationController::createTickedid(){
+	
+}
+	
+void ReservationController::showTicketinformation(){
+	
+}
+	
+void ReservationController::showFlight(){
+	
+}
+
+bool ReservationController::cancelTicketid(string ticketId){
+	
+}
+
+void ReservationController::showCustomer(){
+	
+}
+
+void ReservationController::deldeteFlight(string flightCode){
+	
+}
+
+void ReservationController::addFlight(string start,string destination,string departureTime,string arriveTime,double price,string flightCode){
+	
+}
+
+bool ReservationController::checkin(string ticketId){
+	
+}
+
+void ReservationController::reserveTicketName(string firstname,string lastname,string passportId){
+	
+}
+
--- FlightTicket/UI.cpp
+++ FlightTicket/UI.cpp
@@ -4,3 +4,42 @@ void UI::Register(string firstname,string lastname,string passportId,string user
 	ReservationController *test = new ReservationController();
 	test->Register(firstname,lastname,passportId,username,password);
 }
+void UI::login(string username,string password){
+	
+	
+}
+void UI::reserveTicket(string flightCode ){
+	
+}
+void UI::reserveSeat(string seatId){
+	
+}
+
+void UI::showFlight(){
+	
+}
+
+bool UI::cancelTicketld(string ticketId){
+	
+}
+
+void UI::showCustomer(){
+
+}
+	
+bool UI::deleteFlight(string flightCode){
+	
+}
+void UI::addFlight(string start,string destination,string departureTime,string arriveTime,double price,string flightCode){
+	
+}
+
+bool UI::checkin(string ticketId){
+	
+}
+	
+void UI::reserveTicketName(string firstname,string lastname,string passportId){
+	
+}
+
+
